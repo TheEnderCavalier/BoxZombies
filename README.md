230a4478b01caec32b04e38f8d1606cdb3dd6373
Necross <xendermanstudios@gmail.com>
2019-09-20 13:49:21 -0700

    RNG test KAMPUREE

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # BoxZombies
 This project was originally created as a simple application of my basic skills in Python; however, I have decided to fully rework it to refresh my skills in Python so I can TA the programming class I took 4 years ago as well as I can.
 
-The game is purely automated.
+The game is a roguelike that is played without any human interaction.
+
+## RNG test
+Min: 88 [1, 11, 7, 1, 4, 20, 4, 6, 18, 16]
+Average: 766.141902
+Median: 701.0
+Max: 4110 [697, 282, 957, 271, 51, 48, 849, 26, 29, 900]
+Std Dev: 333.83236893360487
+Seconds taken: 15744.285781145096
--- master.py
+++ master.py
@@ -1,27 +1,27 @@
 import random
 import numpy as np
+import time
 class Gem:
     name=""
     rarity=0
     value=0
     def __init__(self):
-        tiervalues={0:random.randint(1,1000000),0.5:random.randint(10,1000000),
-        0.75:random.randint(100,1000000),0.875:random.randint(1000,1000000),
-        0.9375:random.randint(10000,1000000),0.96875:random.randint(100000,1000000),
-        0.984375:random.randint(1000000,10000000)}
+        tiervalues={0:random.randint(1,10),0.125:random.randint(10,20),
+        0.25:random.randint(20,40),0.5:random.randint(40,80),
+        0.75:random.randint(80,160),0.875:random.randint(160,320),
+        0.984375:random.randint(320,1000)}
         self.name="".join([random.choice("qwertyuiopasdfghjklzxcvbnm") for x in range(random.randint(2,10))]).capitalize()
         rarity=random.random()
         table=[rarity>=x for x in tiervalues.keys()]
         self.rarity=(len(tiervalues)-1)-table[::-1].index(max(table))
         self.value=tiervalues[max([k for k in tiervalues.keys() if rarity>=k])]
-    def unveil(self):
-        print("A gem of {0} with rarity {1} valued at {2}".format(self.name,self.rarity,self.value))
 tests=[]
 mingems=[]
 maxgems=[]
-for testno in range(100000):
+start=time.time()
+for testno in range(1,10001):
     gems=[Gem().value for _ in range(10)]
-    print("{}: {} ({})".format(testno,sum(gems),str(gems)))
+    print("{}: {} {}".format(testno,sum(gems),str(gems)))
     if mingems and maxgems:
         if sum(gems)<min(tests):
             mingems=gems
@@ -31,5 +31,5 @@ for testno in range(100000):
         mingems=gems
         maxgems=gems
     tests.append(sum(gems))
-tests=tests[1:]
-print("Min: {} ({})\nAverage: {}\nMedian: {}\nMax: {} ({})\nStd Dev: {}".format(min(tests),mingems,np.mean(tests),np.median(tests),max(tests),maxgems,np.std(tests)))
+elapsed=time.time()-start
+print("Min: {} {}\nAverage: {}\nMedian: {}\nMax: {} {}\nStd Dev: {}\nSeconds taken: {}".format(min(tests),mingems,np.mean(tests),np.median(tests),max(tests),maxgems,np.std(tests),elapsed))
